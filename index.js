c17bf6bd6505a6f14365fb064abf0127365ee9e0
afahernandes <afahernandes@gmail.com>
2021-08-25 09:40:08 +0700

    hello xpress

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //instantiate express module here
+const express=require('express');
 
 //use express in app variable here
+const app=express();
 
 //define the server port here
-
+const port =6000
 //create the homepage route here and inside it create res means, response, and it send string "Hello Express!" to the API
+app.get('/',(req,res)=>{
+    res.send('Hello expeess');
+});
 
 // Create listen here
+
+app.listen(port,()=>{
+    console.log(`Listening on ${port}`);
+}
+    );
